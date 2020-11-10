754c1e82fb98de92855b0f69298e67a4d126c6db
zhuyichu <74169425+zhuyichu@users.noreply.github.com>
2020-11-10 09:03:09 +0800

    商品表添加注释

--- Model/GoodsModel.cs
+++ Model/GoodsModel.cs
@@ -9,33 +9,33 @@ namespace Model
     /// </summary>
     public class GoodsModel
     {
-        public int GoodsId      { get; set; }
-        public string GoodsName    { get; set; }
-        public int GoodsTid     { get; set; }
-        public int GoodsSid     { get; set; }
-        public string GoodsImg     { get; set; }
-        public string GoodsCode    { get; set; }
-        public int GoodsUid     { get; set; }
-        public int GoodsNum     { get; set; }
-        public decimal GoodsMoney   { get; set; }
-        public int GoodsSize    { get; set; }
-        public string GoodsPeople  { get; set; }
-        public int GoodsState   { get; set; }
+        public int      GoodsId      { get; set; }//商品Id
+        public string   GoodsName    { get; set; }//商品名称
+        public int      GoodsTid     { get; set; }//商品类型Id
+        public int      GoodsSid     { get; set; }//商品供应商Id
+        public string   GoodsImg     { get; set; }//商品图片
+        public string   GoodsCode    { get; set; }//商品编号
+        public int      GoodsUid     { get; set; }//商品单位ID
+        public int      GoodsNum     { get; set; }//商品数量
+        public decimal  GoodsMoney   { get; set; }//商品价钱
+        public int      GoodsSize    { get; set; }//商品体积
+        public string   GoodsPeople  { get; set; }//商品负责人
+        public int      GoodsState   { get; set; }//商品状态  --0下架   1上架
 
         //单位类
-        public int UnitId { get; set; }
-        public string UnitName { get; set; }
-
-        //供应商类
-        public int SuppleId { get; set; }
-        public string SuppleName { get; set; }
-        public string SuppleAdress { get; set; }
-        public string SupplePhone { get; set; }
-        public string SupplePople { get; set; }
+        public int      UnitId       { get; set; }
+        public string   UnitName     { get; set; }
+                        
+        //供应商类      
+        public int      SuppleId     { get; set; }
+        public string   SuppleName   { get; set; }
+        public string   SuppleAdress { get; set; }
+        public string   SupplePhone  { get; set; }
+        public string   SupplePople  { get; set; }
 
         //类型类
-        public int TypedId { get; set; }
-        public string TypedName { get; set; }
+        public int      TypedId      { get; set; }
+        public string   TypedName    { get; set; }
 
     }
 }
